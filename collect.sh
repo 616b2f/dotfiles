bf21e0b4a0aa4bc80fcaeed568e1f51686249d22
Alexej Kowalew <616b2f@gmail.com>
2021-01-19 21:17:15 +0100

    fix rsync

--- collect.sh
+++ collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync ~/.config/sway/ ./.config/sway/
-rsync ~/.config/waybar/ ./.config/waybar/
-rsync ~/.config/nvim/ ./.config/nvim/
-rsync ~/.gitconfig ./.gitconfig
+rsync -r ~/.config/sway/ ./.config/sway/
+rsync -r ~/.config/waybar/ ./.config/waybar/
+rsync -r ~/.config/nvim/ ./.config/nvim/
+rsync -r ~/.gitconfig ./.gitconfig
