7e1f93329b354c4745d0fc86a5205a4d95de525f
Alexej Kowalew <616b2f@gmail.com>
2021-02-10 21:01:55 +0100

    move gitconfig into .config/git

--- collect.sh
+++ collect.sh
@@ -3,4 +3,4 @@
 rsync -r ~/.config/sway/ ./.config/sway/
 rsync -r ~/.config/waybar/ ./.config/waybar/
 rsync -r ~/.config/nvim/ ./.config/nvim/
-rsync -r ~/.gitconfig ./.gitconfig
+rsync -r ~/.config/git/ ./.config/git/
