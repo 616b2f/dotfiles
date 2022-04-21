7b7bad573062997d462b9621026cb9a3661a862c
Alexej Kowalew <616b2f@gmail.com>
2022-04-21 06:42:23 +0200

    update toolbox dev install.sh

--- toolbox/dev/install.sh
+++ toolbox/dev/install.sh
@@ -3,13 +3,14 @@
 # copy additional repos
 sudo cp ../../yum.repos.d/* /etc/yum.repos.d/
 
-sudo dnf update
+sudo dnf update -y
 
 # wl-clipboard also needed for copy from inside neovim
 sudo dnf install -y wl-clipboard
 
 # install neovim nightly
-sudo dnf copr enable agriffis/neovim-nightly
+sudo dnf copr enable agriffis/neovim-nightly -y
+
 # telescope prerequisites
 sudo dnf install -y ripgrep jq
 sudo dnf install -y neovim python3-neovim
@@ -17,20 +18,23 @@ sudo dnf install -y neovim python3-neovim
 # update plugins
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
+# install rust
 # rust prerequisites
 sudo dnf install -y gcc
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
 
 # install golang tooling
 sudo dnf install -y golang
+
+# install dotnet sdk
+sudo dnf install -y dotnet-sdk-6.0
 
 # dependecy for wally-cli
 #sudo dnf install -y libusb
 #go get -u github.com/zsa/wally-cli
 
 # patched font with icons
-sudo dnf install fontconfig
+sudo dnf install -y fontconfig
 TMP_DIR=$(mktemp -d)
 FONT_DIR="$HOME/.local/share/fonts/NerdFonts"
 curl -Lo $TMP_DIR/DejaVuSansMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DejaVuSansMono.zip
