930f95db0e0f4756b101e237103a1a874dab85f4
Alexej Kowalew <616b2f@gmail.com>
2022-06-10 00:41:57 +0200

    kind update and setup

--- install-scripts/kind.sh
+++ install-scripts/kind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TMPDIR=/tmp
 
-VERSION="0.11.1"
+VERSION="0.14.0"
 curl -Lo $TMPDIR/kind "https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-amd64"
-sudo install $TMPDIR/kind ~/.local/bin
+install $TMPDIR/kind ~/.local/bin
--- install.sh
+++ install.sh
@@ -30,3 +30,18 @@ brightnessctl \
 "
 
 rpm-ostree install --allow-inactive --idempotent $PACKAGES
+
+# needed for kind rootless (see https://kind.sigs.k8s.io/docs/user/rootless/)
+mkdir -p /etc/systemd/system/user@.service.d
+cat <<EOF | sudo tee  /etc/systemd/system/user@.service.d/delegate.conf
+[Service]
+Delegate=yes
+EOF
+sudo systemctl daemon-reload
+
+cat <<EOF > /etc/modules-load.d/iptables.conf
+ip6_tables
+ip6table_nat
+ip_tables
+iptable_nat
+EOF
