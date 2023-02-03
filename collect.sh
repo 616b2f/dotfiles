92e0ca8b985c9ebf1eb7469aca066bc97acab13f
Alexej Kowalew <616b2f@gmail.com>
2023-02-03 14:00:16 +0100

    optimize conllect.sh script

--- collect.sh
+++ collect.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 rsync -r ~/.config/git/config ./.config/git/config
-rsync -r --mkpath ~/.config/sway/ ./.config/sway/
-rsync -r --mkpath ~/.config/waybar/ ./.config/waybar/
-rsync -r --mkpath ~/.config/alacritty/ ./.config/alacritty/
-rsync -r --mkpath ~/.config/nvim/ ./.config/nvim/
-rsync -r --mkpath ~/.config/helix/ ./.config/helix/
-rsync -r --mkpath ~/.config/dive/ ./.config/dive/
-rsync -r --mkpath ~/.config/omnisharp/ ./.config/omnisharp/
-rsync -r --mkpath ~/.config/xdg-desktop-portal-wlr/ ./.config/xdg-desktop-portal-wlr/
-rsync -r --mkpath ~/.bashrc.d/ ./.bashrc.d/
+
+folders=".config/helix/ \
+.config/sway/ \
+.config/waybar/ \
+.config/alacritty/ \
+.config/nvim/ \
+.config/dive/ \
+.config/omnisharp/ \
+.config/xdg-desktop-portal-wlr/ \
+.bashrc.d/ \
+"
+
+for folder in $folders
+do
+    if [ -d "$HOME/$folder" ]; then
+        rsync -r --mkpath "$HOME/$folder" "./$folders"
+    fi
+done
