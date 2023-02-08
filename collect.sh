ab745a434673a1c1e3b939c1b7e3589e4d8d6dab
Alexej Kowalew <616b2f@gmail.com>
2023-02-08 22:48:10 +0100

    update collect.sh

--- collect.sh
+++ collect.sh
@@ -2,20 +2,20 @@
 
 rsync -r ~/.config/git/config ./.config/git/config
 
-folders=".config/helix/ \
-.config/sway/ \
-.config/waybar/ \
-.config/alacritty/ \
-.config/nvim/ \
-.config/dive/ \
-.config/omnisharp/ \
-.config/xdg-desktop-portal-wlr/ \
-.bashrc.d/ \
+folders=".config/helix/
+.config/sway/
+.config/waybar/
+.config/alacritty/
+.config/nvim/
+.config/dive/
+.config/omnisharp/
+.config/xdg-desktop-portal-wlr/
+.bashrc.d/
 "
 
 for folder in $folders
 do
     if [ -d "$HOME/$folder" ]; then
-        rsync -r --mkpath "$HOME/$folder" "./$folders"
+        rsync -r --mkpath "$HOME/$folder" "./$folder"
     fi
 done
