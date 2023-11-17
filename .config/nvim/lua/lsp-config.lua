1b8ecbcb5e2ffb7ea76b9b7bff0b8d1506db94a0
Alexej Kowalew <616b2f@gmail.com>
2023-11-17 20:41:33 +0100

    fix border for floats in nvim

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -2,6 +2,20 @@ local lspconfig = require('lspconfig')
 -- local lspconfig_util = require('lspconfig.util')
 require("mason-lspconfig").setup()
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = "single",
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = "single"
+  }
+)
+
 -- local util = require('lspconfig/util')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 local lsp_signature = require('lsp_signature')
