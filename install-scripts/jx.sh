14853a17f66a9f0c0900a81e00329adc88b484ff
Alexej Kowalew <616b2f@gmail.com>
2024-01-28 20:54:12 +0100

    remove jx install script

--- install-scripts/jx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export TMPDIR=`mktemp -d`
-
-VERSION="3.1.240"
-curl -Lo $TMPDIR/jx-cli.tar.gz "https://github.com/jenkins-x/jx-cli/releases/download/v${VERSION}/jx-cli-linux-amd64.tar.gz"
-tar -xzf  $TMPDIR/jx-cli.tar.gz -C $TMPDIR
-
-sudo install $TMPDIR/jx /usr/local/bin
