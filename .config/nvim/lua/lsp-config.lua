3578a2be63bf8466f581949bd4667df14c688262
Alexej Kowalew <616b2f@gmail.com>
2025-04-25 00:52:11 +0200

    update(nvim): change lsp config

--- .config/nvim/lua/lsp-config.lua
+++ .config/nvim/lua/lsp-config.lua
@@ -1,5 +1,3 @@
-local lspconfig = require('lspconfig')
-
 vim.lsp.buf.hover({
   -- Use a sharp border with `FloatBorder` highlights
   border = "single",
@@ -9,212 +7,6 @@ vim.lsp.buf.signature_help({
   -- Use a sharp border with `FloatBorder` highlights
   border = "single",
 })
-
-local on_attach = function(client, bufnr)
-  -- enable inlay hints if LSP server supports it
-  if client.supports_method("textDocument/inlayHint") then
-    vim.lsp.inlay_hint.enable(true, { bufnr=bufnr })
-  end
-
-  -- Set autocommands conditional on server_capabilities
-  if client.supports_method("textDocument/documentHighlight") then
-    vim.api.nvim_exec2([[
-    augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-    ]],
-    {output=true})
-  end
-
-  -- enable semantic tokens highligting hints
-  if client.supports_method("textDocument/semanticTokens") then
-    client.server_capabilities.semanticTokensProvider = true
-  end
-end
-
--- Register a handler that will be called for all installed servers.
--- require("mason-lspconfig").setup_handlers {
---   -- The first entry (without a key) will be the default handler
---   -- and will be called for each installed server that doesn't have
---   -- a dedicated handler.
---   function (server_name) -- default handler (optional)
---     local config = make_config()
---     lspconfig[server_name].setup(config)
---   end,
---
---   -- Next, you can provide a dedicated handler for specific servers.
---   -- ["jdtls"] = function ()
---   --   local mason_registry = require("mason-registry")
---   --   local jdtls = mason_registry.get_package("jdtls") -- note that this will error if you provide a non-existent package name
---   --   local config = make_config()
---   --
---   --   local env = {
---   --     HOME = vim.loop.os_homedir(),
---   --     XDG_CACHE_HOME = os.getenv 'XDG_CACHE_HOME',
---   --     JDTLS_JVM_ARGS = os.getenv 'JDTLS_JVM_ARGS',
---   --   }
---   --
---   --   local function get_cache_dir()
---   --     return env.XDG_CACHE_HOME and env.XDG_CACHE_HOME or lspconfig_util.path.join(env.HOME, '.cache')
---   --   end
---   --
---   --   local function get_jdtls_cache_dir()
---   --     return lspconfig_util.path.join(get_cache_dir(), 'jdtls')
---   --   end
---   --
---   --   local function get_jdtls_config_dir()
---   --     return lspconfig_util.path.join(get_jdtls_cache_dir(), 'config')
---   --   end
---   --
---   --   local function get_jdtls_workspace_dir()
---   --     return lspconfig_util.path.join(get_jdtls_cache_dir(), 'workspace')
---   --   end
---   --
---   --   local function get_jdtls_jvm_args()
---   --     local args = {}
---   --     for a in string.gmatch((env.JDTLS_JVM_ARGS or ''), '%S+') do
---   --       local arg = string.format('--jvm-arg=%s', a)
---   --       table.insert(args, arg)
---   --     end
---   --     return unpack(args)
---   --   end
---   --
---   --   config.cmd = {
---   --     'java',
---   --     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
---   --     '-Dosgi.bundles.defaultStartLevel=4',
---   --     '-Declipse.product=org.eclipse.jdt.ls.core.product',
---   --     '-Dlog.protocol=true',
---   --     '-Dlog.level=ALL',
---   --     '-Xmx1g',
---   --     '--add-modules=ALL-SYSTEM',
---   --     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
---   --     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
---   --     -- 💀
---   --     '-jar', jdtls:get_install_path() .. "/plugins/org.eclipse.equinox.launcher_*.jar",
---   --     '-configuration', get_jdtls_config_dir(),
---   --     -- See `data directory configuration` section in the README
---   --     '-data', get_jdtls_workspace_dir()
---   --   }
---   --   config.root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'})
---   --   -- Here you can configure eclipse.jdt.ls specific settings
---   --   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
---   --   -- for a list of options
---   --   config.settings = {
---   --     java = {}
---   --   }
---   --
---   --   -- Language server `initializationOptions`
---   --   -- You need to extend the `bundles` with paths to jar files
---   --   -- if you want to use additional eclipse.jdt.ls plugins.
---   --   --
---   --   -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
---   --   --
---   --   -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
---   --   config.init_options = {
---   --     bundles = {}
---   --   }
---   --
---   --   -- special case here because we use nvim-jdtls
---   --   require('jdtls').start_or_attach(config)
---   -- end,
---
---   ["lua_ls"] = function ()
---     local config = make_config()
---     config.settings = {
---       Lua = {
---         workspace = {
---           checkThirdParty = false, -- stop asking for config env as openresty
---         },
---         -- Do not send telemetry data containing a randomized but unique identifier
---         telemetry = {
---           enable = false,
---         },
---       }
---     }
---     lspconfig.lua_ls.setup(config)
---   end,
---
---   ["pylsp"] = function ()
---     local config = make_config()
---     config.settings = {
---       pylsp = {
---         plugins = {
---           pycodestyle = {
---             -- ignore = {'W391'},
---             maxLineLength = 100
---           }
---         }
---       }
---     }
---     lspconfig.pylsp.setup(config)
---   end,
---
---   ["omnisharp"] = function ()
---     -- local config = make_config()
---     -- config.settings = {
---     --   RoslynExtensionsOptions = {
---     --     enableDecompilationSupport = true,
---     --     enableImportCompletion = true,
---     --     enableAnalyzersSupport = true
---     --   },
---     --   FormattingOptions = {
---     --     enableEditorConfigSupport = true
---     --   },
---     --   RenameOptions = {
---     --     RenameInComments = true,
---     --     RenameOverloads = true,
---     --     RenameInStrings = true
---     --   }
---     -- }
---     -- lspconfig.omnisharp.setup(config)
---   end,
---
---   ["yamlls"] = function ()
---     local config = make_config()
---     config.settings = {
---       yaml = {
---         completion = true,
---         hover = true,
---         validate = true,
---         schemas = {
---           ['http://json.schemastore.org/github-workflow'] = {
---             '.github/workflows/*.{yml,yaml}',
---           },
---           ["http://json.schemastore.org/kustomization"] = {
---             "kustomization.yaml",
---           },
---           ["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/main/docs-v2/content/en/schemas/v4beta9.json"] = {
---             "skaffold.yaml",
---           },
---           kubernetes = "*.yaml",
---           -- ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.26.9-standalone-strict/all.json"] = {
---           --   "*.yaml",
---           -- },
---         },
---         format = {
---           enable = true
---         },
---         schemaStore = {
---           url = "https://www.schemastore.org/api/json/catalog.json",
---           enable = true
---         }
---       },
---       http = {
---         proxyStrictSSL = true
---       }
---     }
---     lspconfig.yamlls.setup(config)
---   end
--- }
-
--- local config = make_config()
---
--- lspconfig.gdscript.setup(config)
---
---
 
 vim.lsp.config('*', {
   capabilities = {
@@ -236,7 +28,29 @@ vim.lsp.config('*', {
 
 vim.lsp.config('*', {
   capabilities = require('blink.cmp').get_lsp_capabilities(),
-  on_attach = on_attach
+  on_attach = function(client, bufnr)
+    -- enable inlay hints if LSP server supports it
+    if client:supports_method("textDocument/inlayHint") then
+      vim.lsp.inlay_hint.enable(true, { bufnr=bufnr })
+    end
+
+    -- Set autocommands conditional on server_capabilities
+    if client:supports_method("textDocument/documentHighlight") then
+      vim.api.nvim_exec2([[
+      augroup lsp_document_highlight
+          autocmd! * <buffer>
+          autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+          autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+      ]],
+      {output=true})
+    end
+
+    -- enable semantic tokens highligting hints
+    if client:supports_method("textDocument/semanticTokens") then
+      client.server_capabilities.semanticTokensProvider = true
+    end
+  end
 })
 
 vim.lsp.enable({
@@ -249,5 +63,3 @@ vim.lsp.enable({
 -- 'jdtls',
 -- 'gdscript',
 })
-
-
